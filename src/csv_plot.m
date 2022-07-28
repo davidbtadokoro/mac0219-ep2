3489696e44c03d384f484011e926131130504d01
David de Barros Tadokoro <davidbtadokoro@usp.br>
2022-07-28 18:59:10 -0300

    Adds octave script to generate graphs of ompi experiments

---
+++ src/csv_plot.m
@@ -0,0 +1,75 @@
+1;
+
+data_ompi = csvread('./data/mandelbrot_ompi/triple_spiral.csv');
+data_ompi_pth = csvread('./data/mandelbrot_ompi_pth/triple_spiral.csv');
+data_ompi_omp = csvread('./data/mandelbrot_ompi_omp/triple_spiral.csv');
+
+global SIZE = 5;
+global THREADS = 5;
+global colors=['r', 'b', 'y', 'g', 'c', 'm'];
+
+function plot_sequential(data)
+  line(data(1:end, 1),
+       data(1:end, 2),
+       "linewidth", 1,
+       "color", 'r',
+       "marker", ".",
+       "markersize", 8,
+       "clipping", "on");
+
+  xlabel("Number of Nodes");
+  ylabel("Time Elapsed (seconds)");
+  box on;
+  grid on;
+
+endfunction
+
+function plot_size_threads(data)
+  global SIZE;
+  global THREADS;
+  global colors;
+
+  row = 1;
+  for i = 1:THREADS
+    line(data(row:row + SIZE - 1, 2),
+      data(row:row + SIZE - 1, 3),
+      "linewidth", 1,
+      "color", colors(i+1),
+      "marker", ".",
+      "markersize", 8,
+      "clipping", "on");
+
+    row += SIZE;
+  end
+
+  xlabel("Number of Nodes");
+  ylabel("Time Elapsed (seconds)");
+  legend("1 thread", "2 threads", "4 threads", "8 threads", "16 threads", "location", "northwest");
+  box on;
+  grid on;
+
+endfunction
+
+# CLEARING
+close all
+
+# OMPI
+figure(1);
+title("OpenMPI", "FontSize", 22, 'FontName', 'SansSerif');
+plot_sequential(data_ompi);
+saveas(1, "ompi", "jpg");
+close;
+
+# OMPI+PTH
+figure(2)
+title("OpenMPI + Pthreads", "FontSize", 22, 'FontName', 'SansSerif');
+plot_size_threads(data_ompi_pth);
+saveas(2, "ompi_pth", "jpg");
+close;
+
+# OMPI+OMP
+figure(3)
+title("OpenMPI + OpenMP", "FontSize", 22, 'FontName', 'SansSerif');
+plot_size_threads(data_ompi_omp);
+saveas(3, "ompi_omp", "jpg");
+close;

f8898cf7a347d2a7d19583d8e4ca7c50b1aadd24
David de Barros Tadokoro <davidbtadokoro@usp.br>
2022-07-28 19:05:40 -0300

    Updates octave script to generate right legend in ompi+omp plot

--- src/csv_plot.m
+++ src/csv_plot.m
@@ -8,7 +8,7 @@ global SIZE = 5;
 global THREADS = 5;
 global colors=['r', 'b', 'y', 'g', 'c', 'm'];
 
-function plot_sequential(data)
+function plot_ompi(data)
   line(data(1:end, 1),
        data(1:end, 2),
        "linewidth", 1,
@@ -24,7 +24,7 @@ function plot_sequential(data)
 
 endfunction
 
-function plot_size_threads(data)
+function plot_ompi_pth(data)
   global SIZE;
   global THREADS;
   global colors;
@@ -50,26 +50,52 @@ function plot_size_threads(data)
 
 endfunction
 
+function plot_ompi_omp(data)
+  global SIZE;
+  global THREADS;
+  global colors;
+
+  row = 1;
+  for i = 1:THREADS
+    line(data(row:row + SIZE - 1, 2),
+      data(row:row + SIZE - 1, 3),
+      "linewidth", 1,
+      "color", colors(i+1),
+      "marker", ".",
+      "markersize", 8,
+      "clipping", "on");
+
+    row += SIZE;
+  end
+
+  xlabel("Number of Nodes");
+  ylabel("Time Elapsed (seconds)");
+  legend("1 thread", "2 threads", "4 threads", "8 threads", "16 threads", "location", "northeast");
+  box on;
+  grid on;
+
+endfunction
+
 # CLEARING
 close all
 
 # OMPI
 figure(1);
 title("OpenMPI", "FontSize", 22, 'FontName', 'SansSerif');
-plot_sequential(data_ompi);
+plot_ompi(data_ompi);
 saveas(1, "ompi", "jpg");
 close;
 
 # OMPI+PTH
 figure(2)
 title("OpenMPI + Pthreads", "FontSize", 22, 'FontName', 'SansSerif');
-plot_size_threads(data_ompi_pth);
+plot_ompi_pth(data_ompi_pth);
 saveas(2, "ompi_pth", "jpg");
 close;
 
 # OMPI+OMP
 figure(3)
 title("OpenMPI + OpenMP", "FontSize", 22, 'FontName', 'SansSerif');
-plot_size_threads(data_ompi_omp);
+plot_ompi_omp(data_ompi_omp);
 saveas(3, "ompi_omp", "jpg");
 close;
